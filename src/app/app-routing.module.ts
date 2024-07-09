fce855de9a749188987307385c2938e76c61cb67
gabriel12QK <jogar737@gmail.com>
2024-07-09 16:24:12 -0500

    intento de navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CajasComponent } from './cajas/cajas.component';
+import { EscenaComponent } from './escena/escena.component';
+import { CamisasComponent } from './camisas/camisas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cajas',component:CajasComponent},
+  {path: 'taza',component:EscenaComponent},
+  {path: 'camisa',component:CamisasComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
